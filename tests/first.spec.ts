6d559cf68cb18fec693402806fe907dc5c928a7a
ashutester <ashubapat@gmail.com>
2026-04-25 21:34:05 -0700

    Add Playwright CICD workflow

---
+++ tests/first.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect} from '@playwright/test'
+
+test('This is the first test to check CICD',async ({ page })=>{
+    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+})
