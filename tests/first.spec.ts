9975e50aa0111580fb6598307c0cfd97227dbbd4
ashutester <ashubapat@gmail.com>
2026-04-25 22:31:02 -0700

    Added security fixes

--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -1,9 +1,11 @@
 import {test, expect} from '@playwright/test'
 
 test('This is the first test to check CICD',async ({ page })=>{
+    const username = process.env.APP_USERNAME!;
+    const password = process.env.APP_PASSWORD!;
     page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('textbox', { name: 'Username' }).fill(username);
+    await page.getByRole('textbox', { name: 'Password' }).fill(password);
     await page.getByRole('button', { name: 'Login' }).click();
     await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
 })
