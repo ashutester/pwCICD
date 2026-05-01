6da65fc6e9f918b6e25d6aefce34430100a6e262
ashutester <ashubapat@gmail.com>
2026-05-01 08:29:07 -0700

    Added Testing Dashboard

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -43,5 +43,10 @@ export default defineConfig({
       name: 'api',
       testMatch: /.*\.api\.spec\.ts/, // Optional: only run files with .api in the name
     },
+  ],
+  reporter: [
+    ['html'],
+    ['json', { outputFile: 'test-results.json' }],  // ← This is important!
+    ['dot'],
   ],
 });
--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -1,13 +1,9 @@
-import { test, expect } from '@playwright/test';
+/// <reference types="node" />
+import { test, expect } from '../fixtures/auth'
 
-test('This is the first test to check CICD', async ({ page }) => {
-  const username = process.env.APP_USERNAME!;
-  const password = process.env.APP_PASSWORD!;
-  page.goto(
-    'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-  );
-  await page.getByRole('textbox', { name: 'Username' }).fill(username);
-  await page.getByRole('textbox', { name: 'Password' }).fill(password);
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+test('This is the first test to check CICD', async ({ authenticatedPage  }) => {
+
+  await expect(authenticatedPage.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+
 });
+  
